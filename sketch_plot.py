c2bd33ac0c61b86d45cec9542ed36b66807a2f6f
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-01-19 20:37:28 +0000

    scatter plot v vs i^2

--- sketch_plot.py
+++ sketch_plot.py
@@ -10,6 +10,18 @@ InteractiveShell.ast_node_interactivity = "all"
 
 fineBeam = pd.read_csv('fine_beam.csv', header=1, nrows=7)
 
+#Check
 fineBeam
+
+x = fineBeam['current (A)']
+y = fineBeam['voltage (V)']
+
+# Squaring x values (i.e. current/i)
+x_sq = x**2
+
+print(x_sq)
+
+plt.rcParams['figure.figsize'] = [12, 8]
+plt.scatter(x_sq, y)
 
 
