c43c27da937882774ed3821dc7014fae37b50667
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-01-19 20:39:36 +0000

    linregress for best fit

--- sketch_plot.py
+++ sketch_plot.py
@@ -24,4 +24,20 @@ print(x_sq)
 plt.rcParams['figure.figsize'] = [12, 8]
 plt.scatter(x_sq, y)
 
+########### Plot with line of best fit (importing linregress to do this) #############
 
+from scipy.stats import linregress
+
+m, c, r, p, stderr = linregress(x_sq, y)
+
+print(f'Gradient is {m:.2f} +/- {stderr:.2f}, Intercept: {c:.2f}')
+print(f'Correction coefficient: {r:.3f}')
+
+# This line is plotted on the data points:
+startLine_x = x_sq.min() -0.1
+endLine_x = x_sq.max() +0.1
+xlin = np.linspace(startLine_x, endLine_x, 50)
+ylin = m*xlin+c
+
+plt.scatter(x_sq, y)
+plt.plot(xlin, ylin, 'g')
