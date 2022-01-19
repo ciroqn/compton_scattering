f8146cc2bf66aaedf0c4acd445140cd984554dfd
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-01-19 20:43:10 +0000

    best fit with curve_fit

--- sketch_plot.py
+++ sketch_plot.py
@@ -41,3 +41,35 @@ ylin = m*xlin+c
 
 plt.scatter(x_sq, y)
 plt.plot(xlin, ylin, 'g')
+
+########### Plot with curve_fit (importing curve_fit to do this) #############
+
+# curve_fit provides more data on m, c, and covariance. It can fit straight lines, despite its name.
+
+from scipy.optimize import curve_fit
+
+def StrtLineModel(x, m, c):
+    y_lin = m*x+c
+    return (y_lin)
+
+popt, pcov = curve_fit(StrtLineModel, x_sq, y)
+
+print(popt)  # gives m and c values
+print(pcov)  # gives covariance, square errors in m, c
+
+perr = np.sqrt(np.diag(pcov))  # error values in m and c
+
+m_fit = popt[0]
+c_fit = popt[1]
+m_err = perr[0]
+c_err = perr[1]
+
+print('------------------------------------')
+print(f'm = {m_fit:.2f} +/- {m_err:.2f}')
+print(f'c = {c_fit:.2f} +/- {c_err:.2f}')
+print('------------------------------------')
+
+# Plotting data
+
+plt.scatter(x_sq, y)
+plt.plot(x_sq, SrtLineModel(x_sq, m_fit, c_fit), 'g')
