a2ab1b72ee60cab6cd3c914b6adce054efce940f
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-02-09 18:26:54 +0000

    calibration data

--- plotting_graphs.py
+++ plotting_graphs.py
@@ -1,1 +1,24 @@
 # Plotting graphs through angles 30 -150 with 10 deg. increments
+
+%matplotlib inline
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+from bokeh.plotting import figure, output_notebook, show
+
+# These two lines enable formatted printing of Pandas DataFrames
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+
+################################### CALIBRATION DATA AND PLOT (K_ALPHA AND K_BETA)#####################################
+
+calibration = pd.read_csv('calibration-35kv.csv')
+
+calibration.head()
+
+x = calibration['Channel']
+y = calibration['Counts']
+
+plt.rcParams['figure.figsize'] = [12,8]
+plt.scatter(x, y)
