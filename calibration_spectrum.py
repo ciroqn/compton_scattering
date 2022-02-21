555f1581f750c8642318196f5d21301c49044b3f
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-02-21 15:24:07 +0000

    import necessary packages

--- calibration_spectrum.py
+++ calibration_spectrum.py
@@ -1,1 +1,14 @@
+%matplotlib inline
+import numpy as np
+import pandas as pd
 
+import matplotlib.pyplot as plt
+from bokeh.plotting import figure, output_notebook, show
+
+# These two lines enable formatted printing of Pandas DataFrames
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+
+from scipy.optimize import curve_fit
+
+output_notebook()
