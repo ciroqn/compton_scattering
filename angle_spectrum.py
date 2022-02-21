6df2103ac7a6ec4b5c62d0fe8c6df95f0fd93352
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-02-21 15:40:53 +0000

    add function to find e_out

--- angle_spectrum.py
+++ angle_spectrum.py
@@ -11,6 +11,11 @@ from bokeh.plotting import figure, output_notebook, show
 from IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = "all"
 
+# CalculatingE_out for particular bin/channel, n
+def binEnergy(E1, E2, n, n1, n2):
+    frac = (E2-E1)/(n2-n1)
+    return E1 + (n - n1)*frac
+
 
 # FIT K ALPHA TO 150 DEG. RESULT
 
